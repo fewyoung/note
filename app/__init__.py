3a78986fa252d7a980f5a1c47a5721fa6b8d4998
fewyoung <fewyoung@163.com>
2018-04-22 23:29:30 +0800

    sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-#~ from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config
 #~ from flask_login import LoginManager
 
 
-#~ db = SQLAlchemy()
+db = SQLAlchemy()
 bootstrap = Bootstrap()
 #~ login_manager = LoginManager()
 #~ login_manager.session_protection = 'strong'
@@ -16,7 +16,7 @@ def create_app(config_name):
 	app = Flask(__name__)
 	app.config.from_object(config[config_name])
 	
-	#~ db.init_app(app)
+	db.init_app(app)
 	bootstrap.init_app(app)
 	#~ login_manager.init_app(app)
 	
