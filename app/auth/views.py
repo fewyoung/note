71ff6f8aedbb49dc47824059dff42e71dde46056
fewyoung <fewyoung@163.com>
2018-04-23 23:43:55 +0800

    login

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,17 +7,20 @@ from flask_login import login_user, logout_user, login_required
 
 @auth.route('/', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()	
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.username.data).first()
-		if user is not None and user.verify_password(form.password.data):
+	login_form = LoginForm()
+	if login_form.submit.data:
+		if login_form.username.data == '' or login_form.password.data == '':
+			flash('错误：用户名或密码不能为空')	
+	if login_form.submit.data and login_form.validate_on_submit():
+		user = User.query.filter_by(username=login_form.username.data).first()
+		if user is not None and user.verify_password(login_form.password.data):
 			login_user(user)
 			next = request.args.get('next')
 			if next is None or not next.startswith('/'):
 				next = url_for('main.index')
 			return redirect(next)
 		flash('错误：无效的用户名或密码')
-	return render_template('auth/login.html', form=form)
+	return render_template('auth/login.html', login_form=login_form)
 
 @auth.route('/logout')
 @login_required
