6b324d06633977f6877bce05d0055d2fa3670faa
fewyoung <fewyoung@163.com>
2018-05-03 17:55:10 +0800

    1

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,20 +4,20 @@ from wtforms.validators import InputRequired, ValidationError, Length
 #~ from flask import flash
 			
 	
-class ClassifyForm(FlaskForm):
-	classify_id = HiddenField('id')
-	classify_name = StringField('分类名')
-	del_submit = SubmitField('删除')	
-	
-	
-class AddForm(FlaskForm):
-	add_name = StringField('分类名', 
+class CalssifyAddForm(FlaskForm):
+	classify_add_name = StringField('分类名', 
 		validators=[InputRequired(message='分类名不能为空'),
 					Length(max=20,message='分类名不能超过20字符'),], 
 		render_kw={"placeholder":"分类名"})
-	add_submit = SubmitField('确定')
+	classify_add_submit = SubmitField('确定')
 					
 
+class NoteAddForm(FlaskForm):
+	note_add_name = StringField('分类名', 
+		validators=[InputRequired(message='分类名不能为空'),
+					Length(max=20,message='分类名不能超过20字符'),], 
+		render_kw={"placeholder":"分类名"})
+	note_add_submit = SubmitField('确定')
 	
 			
 						
--- app/main/views.py
+++ app/main/views.py
@@ -1,49 +1,65 @@
 from . import main
-from .forms import ClassifyForm, AddForm#, DelForm
+from .forms import CalssifyAddForm, NoteAddForm
 from flask import render_template, request, redirect, url_for#~ from flask import session, flash
 from flask_login import login_required, current_user
-from ..models import db, Classify
+from ..models import db, Classify, Note
 
 
-#分类列表：[添加表单,分页对象]
-def add_classify_forms(page):		
-	user_id = current_user.get_id()		
-	#无分页查询
-	#classify_forms = []
-	#~ classifies = Classify.query.filter_by(user_id=user_id).order_by(Classify.id.desc())	
-	#~ for classify in classifies:	
-		#~ classify_form_temp = ClassifyForm()	
-		#~ classify_form_temp.classify_name.data = classify.classify_name
-		#~ classify_form_temp.classify_id.data = classify.id
-		#~ classify_forms.append(classify_form_temp)		
-	#分页查询	
-	pagination = Classify.query.filter_by(
+#分类区域：[添加分类表单,所有分类的分页对象]
+def classify_area(classify_page):		
+	user_id = current_user.get_id()			
+	#分页对象	
+	classify_pagination = Classify.query.filter_by(
 		user_id=user_id).order_by(
 		Classify.id.desc()).paginate(
-		page, per_page=10,error_out=False)
-	page_items = pagination.items	
-	add_form = AddForm()	
-	#~ return (add_form,classify_forms)
-	return (add_form, pagination)
+		classify_page, per_page=10,error_out=False)
+	classify_add_form = CalssifyAddForm()	
+	return (classify_add_form,
+			classify_pagination)
 
 
-#添加分类表单、所有分类表单列表、所有笔记表单列表、内容表单
-def add_classify_article_content_forms(page):
-	add_form, pagination = add_classify_forms(page)	
-	article_forms = []
+#日志名称区域：[添加日志名称表单,所有日志名称的分页对象]
+def note_area(classify_id, note_page):				
+	#分页对象
+	if classify_id:	
+		note_pagination = Note.query.filter_by(
+			classify_id=classify_id).order_by(
+			Note.id.desc()).paginate(
+			note_page, per_page=10,error_out=False)
+	else:
+		note_pagination = None
+	note_add_form = NoteAddForm()	
+	return (note_add_form,
+			note_pagination)
+
+
+#所有区域
+def all_area(classify_page, classify_id, note_page):
+	classify_add_form, classify_pagination = classify_area(classify_page)	
+	note_add_form, note_pagination = note_area(classify_id, note_page)	
 	content_form = None		
-	return (add_form, pagination, article_forms, content_form)
+	return (classify_add_form,
+			classify_pagination,
+			note_add_form,
+			note_pagination,
+			content_form)
 
 
 @main.route('/note', methods=['GET', 'POST'])
 @login_required
 def note():	
-	(add_form,
-	pagination,
-	article_forms,
-	content_form) = add_classify_article_content_forms(page=1)
-	return render_template('note.html', add_form = add_form,
-							pagination = pagination,
+	classify_add_form, classify_pagination = classify_area(classify_page=1)
+	(classify_add_form,
+		classify_pagination,
+		note_add_form,
+		note_pagination,
+		content_form) = classify_area(classify_page=1,
+										classify_id=None,
+										note_page=1)								
+	note_add_form, note_pagination = note_area(classify_id, note_page) 							
+	return render_template('note.html',
+							classify_add_form = classify_add_form,
+							classify_pagination = classify_pagination,
 							article_forms = article_forms,
 							content_form = content_form)
 	
@@ -52,32 +68,31 @@ def note():
 @login_required
 def classify():
 	#删除分类
-	del_id = request.values.get('del_id')
+	del_id = request.values.get('classify_del_id')
 	if del_id:
 		classify_del = Classify.query.filter_by(id = del_id).first()
 		if classify_del:
 			db.session.delete(classify_del)
 			db.session.commit()
-	add_form = AddForm()		
 	#新增分类，未检测分类名
+	classify_add_form = CalssifyAddForm()		
 	user_id = current_user.get_id()
-	add_name = request.values.get('add_name')
+	classify_add_name = request.values.get('classify_add_name')
 	#ajax提交表单，判断button的data值失效	
 	#if add_form.add_submit.data and add_form.validate_on_submit():
 	#ajax只提交数据，无法通过validate
-	#if add_name:
-	if add_name and add_form.validate_on_submit():	
+	if classify_add_name and classify_add_form.validate_on_submit():	
 		classify_add = Classify()
-		classify_add.classify_name = add_name
+		classify_add.classify_name = classify_add_name
 		classify_add.user_id = user_id
 		db.session.add(classify_add)
 		db.session.commit()	
-	page = request.values.get('page_id', default=1, type=int)
+	classify_page = request.values.get('classify_page_id', default=1, type=int)
 	#必须放在最后，否则不是删除或者增加后的查询结果
-	add_form, pagination = add_classify_forms(page)
+	classify_add_form, pagination = classify_area(classify_page)
 	return render_template('classify.html',
-							add_form = add_form,
-							pagination = pagination)
+							classify_add_form = classify_add_form,
+							classify_pagination = classify_pagination)
 							
 
 
