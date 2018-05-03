8ac8e8664ab014bc3bdca382ca2865bd5e81011b
fewyoung <fewyoung@163.com>
2018-05-03 22:34:35 +0800

    1

--- app/main/forms.py
+++ app/main/forms.py
@@ -12,12 +12,12 @@ class CalssifyAddForm(FlaskForm):
 	classify_add_submit = SubmitField('确定')
 					
 
-class NoteAddForm(FlaskForm):
-	note_add_name = StringField('分类名', 
-		validators=[InputRequired(message='分类名不能为空'),
-					Length(max=20,message='分类名不能超过20字符'),], 
-		render_kw={"placeholder":"分类名"})
-	note_add_submit = SubmitField('确定')
+class TitleAddForm(FlaskForm):
+	title_add_name = StringField('标题名', 
+		validators=[InputRequired(message='标题名不能为空'),
+					Length(max=20,message='标题名不能超过20字符'),], 
+		render_kw={"placeholder":"标题名"})
+	title_add_submit = SubmitField('确定')
 	
 			
 						
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from . import main
-from .forms import CalssifyAddForm, NoteAddForm
+from .forms import CalssifyAddForm, TitleAddForm
 from flask import render_template, request, redirect, url_for#~ from flask import session, flash
 from flask_login import login_required, current_user
 from ..models import db, Classify, Note
@@ -18,49 +18,32 @@ def classify_area(classify_page):
 			classify_pagination)
 
 
-#日志名称区域：[添加日志名称表单,所有日志名称的分页对象]
-def note_area(classify_id, note_page):				
+#标题区域：[添加标题表单,所有标题的分页对象]
+def title_area(classify_id, title_page):				
 	#分页对象
 	if classify_id:	
-		note_pagination = Note.query.filter_by(
+		title_pagination = Note.query.filter_by(
 			classify_id=classify_id).order_by(
 			Note.id.desc()).paginate(
-			note_page, per_page=10,error_out=False)
+			title_page, per_page=10,error_out=False)
 	else:
-		note_pagination = None
-	note_add_form = NoteAddForm()	
-	return (note_add_form,
-			note_pagination)
-
-
-#所有区域
-def all_area(classify_page, classify_id, note_page):
-	classify_add_form, classify_pagination = classify_area(classify_page)	
-	note_add_form, note_pagination = note_area(classify_id, note_page)	
-	content_form = None		
-	return (classify_add_form,
-			classify_pagination,
-			note_add_form,
-			note_pagination,
-			content_form)
+		title_pagination = None
+	title_add_form = TitleAddForm()	
+	return (title_add_form,
+			title_pagination)
 
 
 @main.route('/note', methods=['GET', 'POST'])
 @login_required
 def note():	
 	classify_add_form, classify_pagination = classify_area(classify_page=1)
-	(classify_add_form,
-		classify_pagination,
-		note_add_form,
-		note_pagination,
-		content_form) = classify_area(classify_page=1,
-										classify_id=None,
-										note_page=1)								
-	note_add_form, note_pagination = note_area(classify_id, note_page) 							
+	title_add_form, title_pagination = title_area(classify_id = None, title_page = 1)
+	content_form = None							
 	return render_template('note.html',
 							classify_add_form = classify_add_form,
 							classify_pagination = classify_pagination,
-							article_forms = article_forms,
+							title_add_form = title_add_form,
+							title_pagination = title_pagination,
 							content_form = content_form)
 	
 
@@ -68,9 +51,9 @@ def note():
 @login_required
 def classify():
 	#删除分类
-	del_id = request.values.get('classify_del_id')
-	if del_id:
-		classify_del = Classify.query.filter_by(id = del_id).first()
+	classify_del_id = request.values.get('classify_del_id')
+	if classify_del_id:
+		classify_del = Classify.query.filter_by(id = classify_del_id).first()
 		if classify_del:
 			db.session.delete(classify_del)
 			db.session.commit()
@@ -80,7 +63,7 @@ def classify():
 	classify_add_name = request.values.get('classify_add_name')
 	#ajax提交表单，判断button的data值失效	
 	#if add_form.add_submit.data and add_form.validate_on_submit():
-	#ajax只提交数据，无法通过validate
+	#ajax只提交数据，不进行validate
 	if classify_add_name and classify_add_form.validate_on_submit():	
 		classify_add = Classify()
 		classify_add.classify_name = classify_add_name
@@ -89,16 +72,38 @@ def classify():
 		db.session.commit()	
 	classify_page = request.values.get('classify_page_id', default=1, type=int)
 	#必须放在最后，否则不是删除或者增加后的查询结果
-	classify_add_form, pagination = classify_area(classify_page)
+	classify_add_form, classify_pagination = classify_area(classify_page)
 	return render_template('classify.html',
 							classify_add_form = classify_add_form,
 							classify_pagination = classify_pagination)
 							
 
-
-
-
-
+@main.route('/note/title', methods=['GET', 'POST'])
+@login_required
+def title():
+	#删除标题
+	title_del_id = request.values.get('title_del_id')
+	if title_del_id:
+		title_del = Note.query.filter_by(id = title_del_id).first()
+		if title_del:
+			db.session.delete(title_del)
+			db.session.commit()
+	#新增标题，未检测标题
+	title_add_form = TitleAddForm()
+	classify_id = request.values.get('classify_id')
+	title_add_name = request.values.get('title_add_name')
+	if title_add_name and title_add_form.validate_on_submit():	
+		title_add = Note()
+		title_add.note_title = title_add_name
+		title_add.classify_id = classify_id
+		db.session.add(title_add)
+		db.session.commit()
+	title_page = request.values.get('title_page_id', default=1, type=int)
+	#必须放在最后，否则不是删除或者增加后的查询结果
+	title_add_form, title_pagination = title_area(classify_id, title_page)
+	return render_template('title.html',
+							title_add_form = title_add_form,
+							title_pagination = title_pagination)
 
 
 
